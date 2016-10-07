34fbd303e22a18e2e4b812b0dee2dae0936bd9e6
jacob <2staffi@gmail.com>
2016-10-07 10:27:41 -0400

    fixed output formatting

--- formatter.rb
+++ formatter.rb
@@ -1,12 +1,9 @@
 def fix(names)
     group = ""
-    if names.length == 3
-        names.each do |pair|
+    names.each do |pair|
+        if pair.include? (pair[2])
             group << pair[0] + ", " + pair[1] + ", and " + pair[2] + "<br>"
-        end
-    else
-        names.length == 2
-        names.each do |pair|
+        else
             group << pair[0] + " and " + pair[1] + "<br>"
         end
     end
