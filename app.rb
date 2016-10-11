22f083a995a09d23f817cf2b394e65d01800db28
jacob <2staffi@gmail.com>
2016-10-11 12:29:38 -0400

    working

--- app.rb
+++ app.rb
@@ -8,17 +8,17 @@ end
 
 post '/number' do
     num = params[:student_number]
-    redirect '/names?student_number=' + num
+    redirect '/names?num=' + num
 end
 
 get '/names' do
-    num = params[num]
+    num = params[:num]
     erb :get_names, :locals => {:num => num}
 end
 
 post '/names' do
     num = params[:num]
-    names = params[:user_names].split.map(&:capitalize).join(' ')
+    names = params[:user_names]
     random_names_array = randomizer(names)
     name_pairs = fix(random_names_array)
     erb :get_names_again, :locals => {:name_pairs => name_pairs}
--- random_pair.rb
+++ random_pair.rb
@@ -2,8 +2,8 @@
 
 ### This is the work everyone just stole fromm me ###
 def randomizer(names)
-    names_array = names.split(",")
-    shuffled = names_array.shuffle
+    names = names.map(&:capitalize)
+    shuffled = names.shuffle
     sliced = shuffled.each_slice(2).to_a
     if shuffled.length % 2 == 1
         sliced[-2] << sliced[-1][0]
