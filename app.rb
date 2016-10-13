040446b64531c01a54edb4a078e6c455e7522256
jacob <2staffi@gmail.com>
2016-10-13 10:26:07 -0400

    cleaned up unused get/post

--- app.rb
+++ app.rb
@@ -5,16 +5,6 @@ get '/' do
     erb :number_box
 end
 
-post '/number' do
-    names = params[:myInput]
-    redirect '/names?students=' + names
-end
-
-get '/names' do
-    names = params[:names]
-    erb :get_names, :locals => {:num => num}
-end
-
 post '/names' do
     names = params.values
     random_pairs = random_pair(names)
