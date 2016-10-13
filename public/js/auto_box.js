d43510f5f233300b310bc1c0036b1bdd0756e435
jacob <2staffi@gmail.com>
2016-10-13 19:54:24 -0400

    moved js to auto_box added placeholder

--- public/js/auto_box.js
+++ public/js/auto_box.js
@@ -1,12 +1,15 @@
-function myFunction() {
-    var x = document.getElementById("myInput").value;
-    var y = "";
-    for (i = 0; i < x; i++)
-        document.getElementById("demo").innerHTML = "Enter their names in the boxes below:<br>";
-    for (var i = 1; i <= x; i++) {
-        var input = document.createElement("input");
-        input.type = "text";
-        input.name = "text" + i;
-        document.getElementById("demo").appendChild(input).value;
+    function myFunction() {
+        var x = document.getElementById("myInput").value;
+        var y = "";
+        for (i = 0; i < x; i++)
+            document.getElementById("demo").innerHTML = "Enter their names in the boxes below:<br>";
+        for (var i = 1; i <= x; i++) {
+            var input = document.createElement("input");
+            input.type = "text";
+            input.name = "text" + i;
+            input.id = "marv";
+            input.required = true;
+            input.placeholder = "Enter Name"
+            document.getElementById("demo").appendChild(input).value;
+        }
     }
-}
