bf20430065fe7a4b5bed09cc2579931530366ef4
jacob <2staffi@gmail.com>
2016-10-18 09:53:06 -0400

    removed junk

--- public/js/auto_box.js
+++ public/js/auto_box.js
@@ -1,8 +1,6 @@
     function myFunction() {
         var x = document.getElementById("myInput").value;
-        var y = "";
-        for (i = 0; i < x; i++)
-            document.getElementById("demo").innerHTML = "Enter their names in the boxes below:<br>";
+        document.getElementById("demo").innerHTML = "Enter their names in the boxes below:<br>";
         for (var i = 1; i <= x; i++) {
             var input = document.createElement("input");
             input.type = "text";
